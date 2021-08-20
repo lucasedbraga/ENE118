31c74e8dbaf3766ac78bd1248e119f8ecf07558a
Lucas Eduardo Silva Braga <62674895+lucasedbraga@users.noreply.github.com>
2021-08-20 18:34:37 -0300

    mais exercicios
    mais exerciciosg

---
+++ SUPERVISORIO1/README.txt
@@ -0,0 +1,2 @@
+Para rodar o servidor, basta executar o arquivo: ServidorRefinaria.exe que está dentro da pasta ServidorMODBUS
+Verifique as regras do seu antivírus para que ele não bloqueie a execução do software.
---
+++ SUPERVISORIO1/popups.py
@@ -0,0 +1,30 @@
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+
+
+class ModbusPopup(Popup):
+    """
+    Popup da configuração do MODBUSPopup
+    """
+    _info_lb = None
+    def __init__(self, server_ip,server_port, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.txt_ip.text = str(server_ip)
+        self.ids.txt_porta.text = str(server_port)
+
+    def setInfo(self,message):
+        self._info_lb = Label(text=message)
+        self.ids.layout.add_widget(self._info_lb)
+
+    def clearInfo(self):
+        if self._info_lb is not None:
+            self.ids.layout.remove_widget(self._info_lb)
+
+
+class ScanPopup(Popup):
+    """
+    Popup da configuração do tempo de varredura
+    """
+    def __init__(self,scantime, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.txt_st.text = str(scantime)
